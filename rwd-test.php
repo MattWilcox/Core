c66857974f7e8155da887597d93b17551502d635
Matt Wilcox <mail@matthewwilcox.com>
2012-05-12 12:01:12 +0100

    Made the testing page more useful

--- rwd-test.php
+++ rwd-test.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+	$url = htmlentities($_GET['url'],ENT_QUOTES,"UTF-8");
+?><!DOCTYPE html>
 <html lang=en>
 <head>
 	<meta charset=UTF-8>
@@ -21,30 +23,30 @@
 <body>
 	<div class="breakpoint_1">
 		<h2>Breakpoint 1 (0 to 319)</h2>
-		<iframe src="index.htm" width="319" height="480"></iframe>
+		<iframe src="<?php echo $url ?>" width="319" height="480"></iframe>
 	</div>
 
 	<div class="breakpoint_2">
 		<h2>Breakpoint 2 (320 to 479)</h2>
-		<iframe src="index.htm" width="320" height="480"></iframe>
-		<iframe src="index.htm" width="479" height="480"></iframe>
+		<iframe src="<?php echo $url ?>" width="320" height="480"></iframe>
+		<iframe src="<?php echo $url ?>" width="479" height="480"></iframe>
 	</div>
 
 	<div class="breakpoint_3">
 		<h2>Breakpoint 3 (480 to 699)</h2>
-		<iframe src="index.htm" width="480" height="1024"></iframe>
-		<iframe src="index.htm" width="699" height="1024"></iframe>
+		<iframe src="<?php echo $url ?>" width="480" height="1024"></iframe>
+		<iframe src="<?php echo $url ?>" width="699" height="1024"></iframe>
 	</div>
 
 	<div class="breakpoint_4">
 		<h2>Breakpoint 4 (700 to 959)</h2>
-		<iframe src="index.htm" width="700" height="1024"></iframe>
-		<iframe src="index.htm" width="959" height="1024"></iframe>
+		<iframe src="<?php echo $url ?>" width="700" height="1024"></iframe>
+		<iframe src="<?php echo $url ?>" width="959" height="1024"></iframe>
 	</div>
 
 	<div class="breakpoint_3">
 		<h2>Breakpoint 5 (960 and up)</h2>
-		<iframe src="index.htm" width="960" height="1024"></iframe>
+		<iframe src="<?php echo $url ?>" width="960" height="1024"></iframe>
 	</div>
 </body>
 </html>
